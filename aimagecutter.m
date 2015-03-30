09f51930de8bcab2c6ac92a3123baadc041749f4
James Swineson <jamesswineson@gmail.com>
2015-03-30 12:21:41 +0800

    Add correct GPL license information

--- aimagecutter.m
+++ aimagecutter.m
@@ -1,7 +1,22 @@
 % Automatic image cutter 
-% by James Swineson, 2015-03-29
 % https://github.com/Jamesits/auto-image-cutter
-% Licensed under GPL 2.0
+% 
+% Copyright (C) 2015 James Swineson
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License along
+% with this program; if not, write to the Free Software Foundation, Inc.,
+% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 
 % ======================== Settings ========================
 
@@ -34,6 +49,11 @@ run_length_encoding_minimal_distance = 10;
 % ====================== Settings End ======================
 
 % ========================= Program ========================
+
+disp('Automatic image cutter version 1.0, Copyright (C) 2015 James Swineson');
+disp('Automatic image cutter comes with ABSOLUTELY NO WARRANTY; for details see LICENSE.');
+disp('This is free software, and you are welcome to redistribute it under certain conditions.');
+
 % read source image
 tic;
 disp(strcat('[INFO]Reading image from''', image_path, '''...'));
